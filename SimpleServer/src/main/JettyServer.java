6fc967fbad95da05abe680ef3ea2c54665d1dd26
Seth Theeke <Seth.P.Theeke@leidos.com>
2017-06-10 23:48:20 -0400

    Revert "Revert "Added Simple Server project with example endpoint""
    This reverts commit d8fefe792bda515294a8302d6b2b361c63b8cf0c.

---
+++ SimpleServer/src/main/JettyServer.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class JettyServer{
+
+	public static void main(String[] args) throws Exception{
+		Server server = new Server(8080);
+		
+		ServletContextHandler handler = new ServletContextHandler(server, "/example");
+		handler.addServlet(ExampleServlet.class, "/");
+
+		server.start();
+	}
+}
