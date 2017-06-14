f8db456c10ed29f9b50dc59a2e7b910dc62d1c03
Seth Theeke <Seth.P.Theeke@leidos.com>
2017-06-13 22:55:58 -0400

    Updated Project to use grade build file

--- SimpleServer/src/main/java/server/JettyServer.java
+++ SimpleServer/src/main/java/server/JettyServer.java
@@ -1,7 +1,9 @@
-package main;
+package server;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+
+import servlet.ExampleServlet;
 
 public class JettyServer{
 
--- SimpleServer/src/main/java/servlet/ExampleServlet.java
+++ SimpleServer/src/main/java/servlet/ExampleServlet.java
@@ -1,4 +1,4 @@
-package main;
+package servlet;
 
 import java.io.IOException;
 
